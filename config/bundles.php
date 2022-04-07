bc82fe0444896b39d3fdc28e9254e2aba0d94c3c
urban <uetter@gmail.com>
2022-04-07 22:13:49 +0200

    Remove maker bundle since we're not using it now

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
